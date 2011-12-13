b938b6d41f3a5cfe8d85726d4f2bb3909b807617
Anatoly Lapshin <technologymasta@gmail.com>
2011-12-13 17:43:51 +0400

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hotels::Application.config.secret_token = 'fa0aef613be4d80798da69f6954e95f0dffba007480f0979d3c1667f17a1a22be4ac36b2f60dec1ed5961fead6b3a1047748463acb180b4e593dcd9c41d49c67'
